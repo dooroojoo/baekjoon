47408d052626f6ed6b5142b9707e6f0bb1bf6714
dooroojoo <dooroojoo@kakao.com>
2021-07-03 18:14:10 +0900

    1단계 입출력과 사칙연산

---
+++ 01_InputOutput/no1008.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class no1008 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		double num1 = sc.nextInt();
+		double num2 = sc.nextInt();
+		
+		System.out.println(num1/num2);
+
+	}
+
+}
